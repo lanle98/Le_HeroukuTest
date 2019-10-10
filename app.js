c4b3892cd23038800a0aafff8c78dc2b586ace55
Darren Le <darrenle98@gmail.com>
2019-10-10 05:14:12 -0400

    fixing herouku 3

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname + "/views"));
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
