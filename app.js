d9dd7c4c8ddbf442528bf87e69bc5e9c7ab47e5b
Darren Le <darrenle98@gmail.com>
2019-10-10 04:27:24 -0400

    added products

--- app.js
+++ app.js
@@ -19,10 +19,30 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/sneaker", (req, res) => {
-  res.render("home", {
-    homemessage: "Lan Le",
-    bio: "some generic bio info"
+app.get("/running", (req, res) => {
+  res.render("categories", {
+    name: "Adidas UltraBoost",
+    description:
+      "Rock a fresh but classic look with the all-white Ultraboost Shoes from adidas. Ultraboost technology provides the ultimate in cushioning while the Torison System provides a stable ride for runners of all abilities.",
+    img: `running`
+  });
+});
+
+app.get("/boots", (req, res) => {
+  res.render("categories", {
+    name: "Timberland Boots",
+    description:
+      "When you think of Timberland® boots, you're thinking of these. Our original waterproof boot was designed more than 40 years ago and remains a best-seller today, with tireless waterproof performance and instantly recognizable work-boot styling.",
+    img: `boots`
+  });
+});
+
+app.get("/basketball", (req, res) => {
+  res.render("categories", {
+    name: "Jordan Retro 12",
+    description:
+      "If you’re looking for a top-quality kick for your kid, look no further than the Jordan Retro 12. This sneaker is comfortable, cool, and inspired by the shoe Michael Jordan wore when he won his fifth league title. Your kiddo will start their day on the right foot with the Jordan Retro 12, knowing that they walk in a shoe similar to the one Michael Jordan rocked to the top. Kids will step ahead in the classroom and on the court with the Jordan Retro 12.",
+    img: `basketball`
   });
 });
 
